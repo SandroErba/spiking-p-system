ee066eb4e847cebba39ad48c690bc329a928ca53
Bboy Kata <72092989+BboyKata@users.noreply.github.com>
2026-06-05 18:01:48 +0200

    b

--- sps/m_matrix_executor.py
+++ sps/m_matrix_executor.py
@@ -3,6 +3,8 @@ from sps.spike_utils import TransformationRule
 from sps.m_snp_system import MSNPSystem  
 from sps.snp_system import SNPSystem
 from sps.config import Config
+from sps.m_gpu import MSNPSystemGPU
+
 
 class MatrixExecutor:
 
@@ -19,17 +21,16 @@ class MatrixExecutor:
         # Initialize the vectors and matrices
         configurationVector = np.zeros(neurons_num, dtype=int)
         spikingVector = np.zeros((rule_num,), dtype=int)
-        spikingTransitionMatrix = np.zeros((rule_num, neurons_num), dtype=int) # as explained in the paper
+        spikingTransitionMatrix = np.zeros((rule_num, neurons_num), dtype=int)
         synapsesMatrix = np.zeros((rule_num, neurons_num), dtype=int)      
-        # I implemented these vectors in order to make the system executable
-        ruleVector = np.zeros(rule_num, dtype=int)  # exact E goes in mod, div is  for all rules in this implementation
-        applyingRuleVector = np.zeros((rule_num,), dtype=int)  # which neuron each rule applies to
+        ruleVector = np.zeros(rule_num, dtype=int)
+        applyingRuleVector = np.zeros((rule_num,), dtype=int)
 
         rule_idx = 0
-        input_neurons = [] # index of input neurons, to which the spike train will be applied
+        input_neurons = []
 
         for neuron in neurons:
-            if neuron.neuron_type == 0:  # if it's an input neuron, add it to the list of input neurons
+            if neuron.neuron_type == 0:
                 input_neurons.append(neuron.nid)
 
             configurationVector[neuron.nid] = neuron.charge
@@ -37,22 +38,36 @@ class MatrixExecutor:
             for rule in neuron.transf_rules:
                 spikingTransitionMatrix[rule_idx, neuron.nid] = -rule.source
                 synapsesMatrix[rule_idx, neuron.nid] = 1
+                
                 for target in neuron.targets:
                     spikingTransitionMatrix[rule_idx, target] = rule.target 
-                    synapsesMatrix[rule_idx,target] = 1 if target > 0 else -1
+                    synapsesMatrix[rule_idx, target] = 1 if target > 0 else -1
 
-                ruleVector[rule_idx] = [rule.mod] # exact goes in mod - assume div = 0
+                # Handle rule.mod - potrebbe essere lista o singolo valore
+                if isinstance(rule.mod, list):
+                    ruleVector[rule_idx] = rule.mod[0] if rule.mod else 0
+                else:
+                    ruleVector[rule_idx] = rule.mod
+                    
                 applyingRuleVector[rule_idx] = neuron.nid
                 rule_idx += 1
 
-        return MSNPSystemGPU(configurationVector, 
-                            spikingVector, 
-                            spikingTransitionMatrix, 
-                            synapsesMatrix, 
-                            ruleVector, 
-                            max_steps, 
-                            deterministic, 
-                            single_spike_train = SNPSystem.spike_train if Config.MODE != "CNN" else None, 
-                            input_neurons=input_neurons, 
-                            synapsesMatrix=synapsesMatrix, 
-                            applyingRuleVector=applyingRuleVector)
+        # Determina single_spike_train in base alla modalità
+        single_spike_train = None
+        if Config.MODE != "CNN":
+            single_spike_train = SNPSystem.spike_train
+
+        # Crea e ritorna l'istanza di MSNPSystemGPU (versione CuPy)
+        # NOTA: NON passare synapsesMatrix due volte!
+        return MSNPSystemGPU(
+            configurationVector=configurationVector,
+            spikingVector=spikingVector,
+            spikingTransitionMatrix=spikingTransitionMatrix,
+            synapsesMatrix=synapsesMatrix,
+            ruleVector=ruleVector,
+            max_steps=max_steps,
+            deterministic=deterministic,
+            single_spike_train=single_spike_train,
+            input_neurons=input_neurons,
+            applyingRuleVector=applyingRuleVector
+        )
